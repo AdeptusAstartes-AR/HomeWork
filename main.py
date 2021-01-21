b8f44098795347dbb2d5e5d24b721b43d689e383
George Sukasyan <1day4evri@rambler.ru>
2021-01-21 23:17:57 +0300

    f-string

--- main.py
+++ main.py
@@ -7,12 +7,24 @@ def f(x):
 text = f" Дорогой {name.lower()}  {midname.upper()}, баланс вашего лицевого счета составлет {f(5)} руб."
 print(text)
 """
-
+"""
 d = {
 "name" : 'Семен',
 "midname" : "Семенович",
 "balance" : 36.82
 }
-text = f" Дорогой {d['name']}  {d['midname']}, баланс вашего лицевого счета составлет {d['balance']} руб."
+text = f" Дорогой {d['name']}  {d['midname']}, баланс вашего лицевого счета составлет {d.get('balance')} руб."
 print(text)
-print(text)
+"""
+gender = {
+    'm': 'Дорогой',
+    'f': 'Дорогая'
+}
+a = (
+    ['Седа', 'Ашотовна', 32.56,'f'],
+    ['Евгений', 'Леонидович', 25.78, 'm'],
+    ['Александр', 'Константинович', 128.93, 'm']
+)
+for name, midname, balance, gen in a:
+    text = f" {gender[gen]} {name}  {midname}, баланс вашего лицевого счета составлет {balance} руб."
+    print(text)
