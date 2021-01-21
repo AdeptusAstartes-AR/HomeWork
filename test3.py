f13d36519c9e2e295c3af963f0e0b9d221ed61a6
George Sukasyan <1day4evri@rambler.ru>
2021-01-21 23:47:14 +0300

    first again

---
+++ test3.py
@@ -0,0 +1,8 @@
+price_rub = 1000
+eur_cost = 88
+price_euro = price_rub / eur_cost
+greetings = f"I am the first programm"
+#type_of_variable
+print(f'This is price in rub: {price_rub} and this is price in euro: {price_euro}')
+print(greetings)
+print(f'')
